b18fb467967a5287be5ac254edd680af861c9e7b
nemoeslovo <nitropolis@gmail.com>
2013-05-06 16:10:59 +0400

    add handy init

--- FlashToCocosARC/FTCPresetPart.m
+++ FlashToCocosARC/FTCPresetPart.m
@@ -16,4 +16,19 @@
 
 @synthesize animationName = _animationName;
 @synthesize numberOfRepetitions = _numberOfRepetitions;
+
+
++ (id)partNamed:(NSString *)_name repeated:(NSInteger)_count {
+    return [[self alloc] initWithAnimationName:_name andCount:_count];
+}
+
+- (id)initWithAnimationName:(NSString *)_name andCount:(NSInteger)_count {
+    self = [super init];
+    if (self) {
+        _animationName       = _name;
+        _numberOfRepetitions = _count;
+    }
+    return self;
+}
+
 @end
